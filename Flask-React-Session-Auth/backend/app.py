e5206cdd174cb5469a7f9112aff9b028c9b47bcf
Zaw Thet Paing <zawthetpaing333@gmail.com>
2024-08-25 11:10:40 +0630

    Flask-React-Session-Auth_sessionAddBackendComplete

--- Flask-React-Session-Auth/backend/app.py
+++ Flask-React-Session-Auth/backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
+from flask_session import Session
 from flask_bcrypt import Bcrypt
 from config import ApplicationConfig
 from models import db, User
@@ -7,11 +8,24 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
-
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+@app.route('/@me')
+def get_current_user():
+    user_id = session.get('user_id')
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
 
 @app.route('/register', methods=['POST'])
 def register_user():
@@ -42,6 +56,8 @@ def login_user():
     
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Unauthorized"}), 401
+    
+    session['user_id'] = user.id
     
     return jsonify({
         "id": user.id,
